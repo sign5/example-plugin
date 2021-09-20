1d350b6aa78bd22067340695e251b32dea5378c8
GitHub Action <action@github.com>
2021-09-20 13:37:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/sign5/exampleplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sign5/exampleplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sign5.exampleplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sign5.exampleplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/sign5/exampleplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sign5/exampleplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sign5.exampleplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sign5.exampleplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
